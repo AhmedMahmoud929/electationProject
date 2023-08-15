a7c5562c1681d251926d43b41ee92c7699cf35e2
Ahmed Mahmoud <97903655+AhmedMahmoud929@users.noreply.github.com>
2023-08-15 12:43:55 +0300

    Update elector.router.js

--- routers/elector.router.js
+++ routers/elector.router.js
@@ -12,13 +12,18 @@ const multer = require("multer");
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, '/tmp/upload'); // Use the /tmp directory for temporary file storage
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  destination: (req, file, cb) => {
+    const uploadFolder = '/tmp/upload'; // Specify the upload folder
+    // Ensure the upload folder exists, create if it doesn't
+    if (!fs.existsSync(uploadFolder)) {
+      fs.mkdirSync(uploadFolder, { recursive: true });
     }
+    cb(null, uploadFolder);
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
 });
 let upload = multer({ storage: storage });
 
